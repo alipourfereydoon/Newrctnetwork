ab228ce4ac98da3d99227a806ba067126ec9a22f
ali.pourfereydoon <pourfereydoonali@gmail.com>
2026-06-05 09:26:12 -0700

    add templates folder to the project

--- account_app/views.py
+++ account_app/views.py
@@ -3,12 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-user = [
-    'ali','jakob','abtin'
-]
+
 
 def account(request,username):
-    if username in user:
-        return HttpResponse(f"my account is{username} ") 
-    else:
-        return HttpResponse("this user is not found")    
+    return render(request,'account_app/account.html')  
